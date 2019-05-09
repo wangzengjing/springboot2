584c58c54002dbe7fa7dcf63c7a35bcaf5d299f9
wangzengjing <1197181836@qq.com>
2019-05-09 21:13:21 +0800

    1.新增God类 2.修改Test文件

---
+++ src/main/java/com/example/demo/God.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class God {
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
 public class Test {
+    int a;
+    static {
+        System.out.println("66666");
+    }
     public static void main(String[] args) {
         System.out.println(66);
     }
